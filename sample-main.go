22ac9324e21fd7cf005dcce3778f9478bf465732
sona-tar <sona.zip@gmail.com>
2015-06-14 13:55:11 +0000

    Add sample-main.go sample-print.go

---
+++ sample-main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+     SamplePrint("test 1")
+     SamplePrint("test 2")
+}
---
+++ sample-print.go
@@ -0,0 +1,7 @@
+package main
+
+import  "fmt"
+
+func SamplePrint(msg string) {
+     fmt.Printf("go-sample %s\n", msg)
+}
